3c71afb6ea315d4538b7ad66ff2fb8452005d254
MaxSun <sundai.cool@hotmail.com>
2020-09-10 23:08:49 +0800

    menu
    add menu

--- routes/index.js
+++ routes/index.js
@@ -7,20 +7,56 @@ var wechatAPI = require('wechat-api');
 const config = require('../config');
 var Utils = require('../utils/utils')
 var talk = require('../services/robot')
-var api = new wechatAPI(config.appid, config.appsecret);
+var expect = require('expect.js');
 
+var api = new wechatAPI(config.appid, config.appsecret);
+const menuConfig = {
+    "button": [
+        {
+            "type": "view",
+            "name": "测试链接1",
+            "url": "http://www.sunsd.cn/"
+        },
+        {
+            "name": "测试链接2",
+            "sub_button": [
+                {
+                    "type": "view",
+                    "name": "测试链接2-1",
+                    "url": "http://www.sunsd.cn/"
+                },
+                {
+                    "type": "view",
+                    "name": "测试链接2-2",
+                    "url": "http://www.sunsd.cn/"
+                }
+            ]
+        }
+    ]
+};
 /* GET home page. */
 router.get('/wechat', function(req, res, next) {
-  var resl=Utils.getSignature(config,req.query)
-  res.send(resl);
+    var resl=Utils.getSignature(config,req.query)
+    res.send(resl);
 });
 
 /* GET home page. */
 router.get('/talk', function(req, res, next) {
-      talk(req.query.talk).then(resl=>{
+    talk(req.query.talk).then(resl=>{
         res.send(resl);
         logger.log("info", "robot talk result:"+resl);
     })
+});
+/* GET home page. */
+router.get('/menu', function(req, res, next) {
+    var menu = JSON.stringify(menuConfig);
+    api.createMenu(menu, function (err, result) {
+        expect(err).not.to.be.ok();
+        expect(result).to.have.property('errcode', 0);
+        expect(result).to.have.property('errmsg', 'ok');
+        done();
+    });
+    res.send("createMenu success");
 });
 
 router.post('/wechat', wechat(config, wechat.text(function (message, req, res, next) {
