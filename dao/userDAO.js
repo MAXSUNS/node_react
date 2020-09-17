568b323232f40c336cf7d3c1fe71ed2af293ed56
MaxSun <sundai.cool@hotmail.com>
2020-09-17 23:39:15 +0800

    Update userDAO.js

--- dao/userDAO.js
+++ dao/userDAO.js
@@ -5,43 +5,52 @@ var config = require('../config');
 var $sql = require('./userSql');
 
 var pool = mysql.createPool( config.sqlConfig );
+const log4js = require('../utils/log4js');
+const logger = log4js.getLogger();
 
 module.exports = {
     add: function (userInfo) {
         pool.getConnection(function(err, connection) {
+            logger.info("add user :"+JSON.stringify(userInfo))
+            let result=queryByOpenid(userInfo.openid);
+            logger.info("search result:"+JSON.stringify(result))
+            if (result){
+                return 'add success';
+            }
             connection.query($sql.insert, [userInfo.nickname, userInfo.password, userInfo.gender, userInfo.openid], function(err, result) {
-                if(err) {
-                    res.send(err);
-                }else{
-                    res.send('add success');
-                }
 
                 // 释放连接
                 connection.release();
+                if(err) {
+                    return err
+                }else{
+                    return 'add success';
+                }
+
             });
         });
     },
     queryAll: function (page,count) {
         pool.getConnection(function(err, connection) {
             connection.query($sql.queryAll, [page, count], function(err, result) {
-                if(err) {
-                    res.send(err);
-                }else{
-                    res.send(result);
-                }
                 connection.release();
+                if(err) {
+                    return err
+                }else{
+                    return result;
+                }
             });
         });
     },
     queryByOpenid: function (openid) {
         pool.getConnection(function(err, connection) {
             connection.query($sql.queryByOpenid, [openid], function(err, result) {
-                if(err) {
-                    res.send(err);
-                }else{
-                    res.send(result);
-                }
                 connection.release();
+                if(err) {
+                    return err
+                }else{
+                    return result;
+                }
             });
         });
     },
