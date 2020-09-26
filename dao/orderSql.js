cc04fb2a4114e81bb46de106781299238ca87567
MaxSun <sundai.cool@hotmail.com>
2020-09-26 21:07:43 +0800

    Update orderSql.js

--- dao/orderSql.js
+++ dao/orderSql.js
@@ -2,7 +2,7 @@ var order = {
     insert:'insert into litemall_order ( user_id, consignee,mobile, address) values (?,?,?,?)',
     updateAddress:'update litemall_order set consignee=?,mobile=?,address=? where id=?',
     delete: 'delete from litemall_order where id=?',
-    queryByUserid: 'select * from litemall_order where user_id=?',
+    queryByUserid: 'select a.*,b.goods_name,b.pic_url from litemall_order a left join litemall_order_goods b on a.id = b.order_id where a.user_id=? group by a.id;',
     queryAll: 'select * from litemall_order limit ?,?'
 };
 
