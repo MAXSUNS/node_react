636221ffeca3d0e35219389ba957cfc813b9ba7e
sundai <sundai.cool@hotmail.com>
2020-10-15 14:37:25 +0800

    build basic

--- app.js
+++ app.js
@@ -20,9 +20,6 @@ app.use('/api', index);
 app.use('/access', access);
 
 
-
-
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -35,7 +32,6 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
