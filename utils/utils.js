946f41a01184360dc2c2a61e7589473a8421e153
MaxSun <sundai.cool@hotmail.com>
2020-10-27 22:37:21 +0800

    update util

--- utils/utils.js
+++ utils/utils.js
@@ -20,7 +20,7 @@ var utils = {
  * @returns {Query|*|{$inc}}
  */
 utils.encrypt = function(str, secret) {
-    var cipher = crypto.createCipher('aes192', secret);
+    var cipher = crypto.createCipheriv('aes192', secret);
     var enc = cipher.update(str, 'utf8', 'hex');
     enc += cipher.final('hex');
     return enc;
@@ -33,7 +33,7 @@ utils.encrypt = function(str, secret) {
  * @returns {Query|*|{$inc}}
  */
 utils.decrypt = function(str, secret) {
-    var decipher = crypto.createDecipher('aes192', secret);
+    var decipher = crypto.createDecipheriv('aes192', secret);
     var dec = decipher.update(str, 'hex', 'utf8');
     dec += decipher.final('utf8');
     return dec;
