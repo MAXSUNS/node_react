5fbed02ed2a63fb13ff85adac7f8f0f42e0a0130
MaxSun <sundai.cool@hotmail.com>
2020-11-28 21:12:49 +0800

    Update goods.js

--- services/goods.js
+++ services/goods.js
@@ -1,8 +1,5 @@
 const log4js = require('../utils/log4js');
 const logger = log4js.getLogger();
-const goodsDAO = require('../dao/goodsDAO');
-const orderDAO = require('../dao/orderDAO');
-const orderGoodsDAO = require('../dao/orderGoodsDAO');
 
 var addOrder=function(goodId,userId) {
 
